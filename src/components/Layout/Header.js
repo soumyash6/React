d60c2ccba046beef31902800566f7665f0b6e0a1
soumya <soumyash651@gmail.com>
2019-09-21 00:11:58 +0530

    all working

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+          <a className="navbar-brand" href="Dashboard.html">
+            Personal Project Management Tool
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <React.Fragment>
+                  <Link to="/dashboard" className="navbar-brand">
+                    DASHBOARD
+                  </Link>
+                </React.Fragment>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link " href="register.html">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+export default Header;
